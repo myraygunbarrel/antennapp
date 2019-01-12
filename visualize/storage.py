779076216ccbe0602107804644e6ac6cd4534fd4
ruslan <rh.voronov@gmail.com>
2019-01-12 17:02:18 +0300

    redis support

--- visualize/storage.py
+++ visualize/storage.py
@@ -1,12 +1,13 @@
 import redis
 import os
+from django.conf import settings
 
 
 class Storage:
     conn = None
 
     def __init__(self):
-        self.conn = redis.from_url(os.getenv('REDIS_URL', 'localhost'))
+        self.conn = redis.from_url(settings.REDIS_URL)
 
     def set(self, key, value):
         self.conn.set(key, value)
